8432e3010ef4cc6821d87a40e6a9a088a283db4f
bashayerhijji <bashayer.hijji@gmail.com>
2018-11-21 08:27:20 +0300

    project files

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:chat_app/LoginPage.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+   runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return new MaterialApp(
+      title: "Login Page",
+      theme: new ThemeData(
+        primarySwatch: Colors.indigo
+      ),
+      home: new LoginPage()
+    );
+  }
+}
